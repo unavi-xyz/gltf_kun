51dbd56dd4aa7bf7aac08053c3fc6c907dfd2f8e
Kayh <basedlainpilled@gmail.com>
2023-11-08 16:10:15 -0500

    init repo

---
+++ default.nix
@@ -0,0 +1,42 @@
+{ lib, pkgs, system, build_inputs, native_build_inputs, makeRustPlatform }:
+let
+  wasmTarget = "wasm32-unknown-unknown";
+
+  rustBin = pkgs.rust-bin.stable.latest.default;
+  rustBinWasm = rustBin.override { targets = [ wasmTarget ]; };
+
+  rustPlatform = makeRustPlatform {
+    cargo = rustBin;
+    rustc = rustBin;
+  };
+
+  rustPlatformWasm = makeRustPlatform {
+    cargo = rustBinWasm;
+    rustc = rustBinWasm;
+  };
+
+  common = {
+    version = "0.0.0";
+    src = ./.;
+    cargoLock.lockFile = ./Cargo.lock;
+    PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+
+    buildInputs = build_inputs;
+    nativeBuildInputs = native_build_inputs;
+
+    LD_LIBRARY_PATH = lib.makeLibraryPath build_inputs;
+  };
+in {
+  cli = rustPlatform.buildRustPackage (common // { pname = "gltf_kun_cli"; });
+  core = rustPlatform.buildRustPackage (common // { pname = "gltf_kun"; });
+  wasm = rustPlatformWasm.buildRustPackage (common // {
+    pname = "gltf_kun";
+    buildPhase = ''
+      cargo build --target ${wasmTarget} --profile wasm-release
+    '';
+    installPhase = ''
+      mkdir -p $out/lib
+      cp target/${wasmTarget}/wasm-release/*.wasm $out/lib/
+    '';
+  });
+}
