4daea74f93268f964da26b82f0c850b8fbeb31e1
Kayh <basedlainpilled@gmail.com>
2024-01-05 03:30:24 -0500

    add gltf example

--- gltf_kun/examples/gltf.rs
+++ gltf_kun/examples/gltf.rs
@@ -1,23 +1,23 @@
-use gltf_kun::format::{glb::GlbFormat, IoFormat};
+use gltf_kun::format::{gltf::GltfFormat, IoFormat};
 
 const CARGO_MANIFEST_DIR: &str = env!("CARGO_MANIFEST_DIR");
 const ASSETS_DIR: &str = "../assets";
-const MODEL: &str = "BoxTextured.glb";
+const MODEL: &str = "TriangleWithoutIndices/TriangleWithoutIndices.gltf";
 
 fn main() {
     let path = format!("{}/{}/{}", CARGO_MANIFEST_DIR, ASSETS_DIR, MODEL);
     println!("Loading: {}", path);
 
     let file = std::fs::File::open(path).expect("Failed to open file");
-    let glb = gltf::Glb::from_reader(file).expect("Failed to parse glTF");
+    let json = serde_json::from_reader(file).expect("Failed to parse glTF");
+    let blob = None;
 
-    let format = GlbFormat(glb);
+    let format = GltfFormat { json, blob };
     let doc = format.import().expect("Failed to import glTF");
 
     doc.nodes().iter().for_each(|node| {
         println!("Node: {:?}", node);
     });
 
-    let exported = GlbFormat::export(doc).expect("Failed to export glTF");
-    let _ = exported.0.to_vec().expect("Failed to convert to bytes");
+    let exported = GltfFormat::export(doc).expect("Failed to export glTF");
 }
