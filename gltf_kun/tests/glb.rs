8b00240e7b295e1490c3c9551bdf8558b8e923ee
Kayh <basedlainpilled@gmail.com>
2024-01-10 12:13:22 -0500

    add asserts to export tests

--- gltf_kun/tests/glb.rs
+++ gltf_kun/tests/glb.rs
@@ -9,5 +9,15 @@ const MODEL: &str = "BoxTextured.glb";
 fn main() {
     let path = format!("{}/{}/{}", CARGO_MANIFEST_DIR, ASSETS_DIR, MODEL);
     let doc = GlbFormat::import_file(&path).expect("Failed to import glTF");
-    let _out = GlbFormat::export(doc).expect("Failed to export glTF");
+    let out = GlbFormat::export(doc).expect("Failed to export glTF");
+
+    let (doc, _, _) = gltf::import_slice(&out.0).expect("Failed to import exported glb");
+
+    assert!(doc.accessors().len() > 0);
+    assert!(doc.buffers().len() > 0);
+    assert!(doc.images().len() > 0);
+    assert!(doc.materials().len() > 0);
+    assert!(doc.meshes().len() > 0);
+    assert!(doc.nodes().len() > 0);
+    assert!(doc.scenes().len() > 0);
 }
--- gltf_kun/tests/gltf.rs
+++ gltf_kun/tests/gltf.rs
@@ -9,5 +9,12 @@ const MODEL: &str = "TriangleWithoutIndices/TriangleWithoutIndices.gltf";
 fn main() {
     let path = format!("{}/{}/{}", CARGO_MANIFEST_DIR, ASSETS_DIR, MODEL);
     let doc = GltfFormat::import_file(&path).expect("Failed to import glTF");
-    let _out = GltfFormat::export(doc).expect("Failed to export glTF");
+    let out = GltfFormat::export(doc).expect("Failed to export glTF");
+
+    assert!(!out.json.accessors.is_empty());
+    assert!(!out.json.buffer_views.is_empty());
+    assert!(!out.json.buffers.is_empty());
+    assert!(!out.json.meshes.is_empty());
+    assert!(!out.json.nodes.is_empty());
+    assert!(!out.json.scenes.is_empty());
 }
