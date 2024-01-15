02d8e387b555caa6ce660e82ce956b7961ed2ff1
Kayh <basedlainpilled@gmail.com>
2024-01-15 13:28:27 -0500

    attribute conversion clean up

--- bevy_gltf_kun/src/import/gltf/primitive.rs
+++ bevy_gltf_kun/src/import/gltf/primitive.rs
@@ -1,8 +1,14 @@
-use bevy::{prelude::*, render::mesh::VertexAttributeValues};
+use bevy::{
+    prelude::*,
+    render::{
+        mesh::{MeshVertexAttribute, VertexAttributeValues},
+        render_resource::VertexFormat,
+    },
+};
 use gltf_kun::graph::gltf::{
     accessor::{
         colors::ReadColors, iter::AccessorIter, joints::ReadJoints, tex_coords::ReadTexCoords,
-        weights::ReadWeights, ComponentType, GetAccessorIterError, Type,
+        weights::ReadWeights, Accessor, ComponentType, GetAccessorIterError, Type,
     },
     primitive::{Primitive, Semantic},
 };
@@ -22,62 +28,108 @@ enum ConversionMode {
 }
 
 #[derive(Debug, Error)]
-pub enum ImportPrimitiveError {
-    #[error("Failed to get accessor iterator: {0}")]
-    GetAccessorIterError(#[from] GetAccessorIterError),
-    #[error("Unsupported attribute format: {0:?} {1:?}")]
-    UnsupportedAttributeFormat(ComponentType, Type),
-}
+pub enum ImportPrimitiveError {}
 
 pub fn import_primitive(
     context: &mut ImportContext,
     p: &Primitive,
 ) -> Result<(), ImportPrimitiveError> {
     for (semantic, accessor) in p.attributes(&context.doc.0) {
-        let (attribute, conversion) = match semantic {
-            Semantic::Normals => (Mesh::ATTRIBUTE_NORMAL, ConversionMode::Any),
-            Semantic::Positions => (Mesh::ATTRIBUTE_POSITION, ConversionMode::Any),
-            Semantic::Tangents => (Mesh::ATTRIBUTE_TANGENT, ConversionMode::Any),
-            Semantic::Colors(0) => (Mesh::ATTRIBUTE_COLOR, ConversionMode::Rgba),
-            Semantic::TexCoords(0) => (Mesh::ATTRIBUTE_UV_0, ConversionMode::TexCoord),
-            Semantic::TexCoords(1) => (Mesh::ATTRIBUTE_UV_1, ConversionMode::TexCoord),
-            Semantic::Joints(0) => (Mesh::ATTRIBUTE_JOINT_INDEX, ConversionMode::JointIndex),
-            Semantic::Weights(0) => (Mesh::ATTRIBUTE_JOINT_WEIGHT, ConversionMode::JointWeight),
-            _ => continue,
-        };
-
-        let buffer_view = match accessor.buffer_view(&context.doc.0) {
-            Some(buffer_view) => buffer_view,
-            None => {
-                warn!("Primitive attribute BufferView not found");
+        let (attribute, values) = match convert_attribute(context, &semantic, &accessor) {
+            Ok(a) => a,
+            Err(err) => {
+                warn!("Failed to convert attribute: {}", err);
                 continue;
             }
-        };
-
-        let buffer = match buffer_view.buffer(&context.doc.0) {
-            Some(buffer) => buffer,
-            None => {
-                warn!("Primitive attribute Buffer not found");
-                continue;
-            }
-        };
-
-        let iter = accessor.iter(&context.doc.0, &buffer_view, &buffer)?;
-
-        let converted = match conversion {
-            ConversionMode::Any => convert_any_values(iter)?,
-            ConversionMode::Rgba => convert_rgba_values(iter)?,
-            ConversionMode::JointIndex => convert_joint_index_values(iter)?,
-            ConversionMode::JointWeight => convert_joint_weight_values(iter)?,
-            ConversionMode::TexCoord => convert_tex_coord_values(iter)?,
         };
     }
 
     Ok(())
 }
 
+#[derive(Debug, Error)]
+enum AttributeConversionError {
+    #[error("Buffer view not found for attribute {0:?}")]
+    BufferViewNotFound(String),
+    #[error("Buffer not found for attribute {0:?}")]
+    BufferNotFound(String),
+    #[error("Failed to get accessor iterator: {0}")]
+    GetAccessorIterError(#[from] GetAccessorIterError),
+    #[error("Unsupported attribute format: {0:?} {1:?}")]
+    UnsupportedAttributeFormat(ComponentType, Type),
+    #[error("Unsupported semantic: {0:?}")]
+    UnsupportedSemantic(Semantic),
+    #[error("Wrong format for attribute {0:?} (expected {1:?}, got {2:?})")]
+    WrongFormat(String, VertexFormat, String, VertexFormat),
+}
+
+fn convert_attribute(
+    context: &ImportContext,
+    semantic: &Semantic,
+    accessor: &Accessor,
+) -> Result<(MeshVertexAttribute, VertexAttributeValues), AttributeConversionError> {
+    let (attribute, conversion) = match semantic {
+        Semantic::Normals => (Mesh::ATTRIBUTE_NORMAL, ConversionMode::Any),
+        Semantic::Positions => (Mesh::ATTRIBUTE_POSITION, ConversionMode::Any),
+        Semantic::Tangents => (Mesh::ATTRIBUTE_TANGENT, ConversionMode::Any),
+        Semantic::Colors(0) => (Mesh::ATTRIBUTE_COLOR, ConversionMode::Rgba),
+        Semantic::TexCoords(0) => (Mesh::ATTRIBUTE_UV_0, ConversionMode::TexCoord),
+        Semantic::TexCoords(1) => (Mesh::ATTRIBUTE_UV_1, ConversionMode::TexCoord),
+        Semantic::Joints(0) => (Mesh::ATTRIBUTE_JOINT_INDEX, ConversionMode::JointIndex),
+        Semantic::Weights(0) => (Mesh::ATTRIBUTE_JOINT_WEIGHT, ConversionMode::JointWeight),
+        _ => {
+            return Err(AttributeConversionError::UnsupportedSemantic(
+                semantic.clone(),
+            ));
+        }
+    };
+
+    let buffer_view = match accessor.buffer_view(&context.doc.0) {
+        Some(buffer_view) => buffer_view,
+        None => {
+            return Err(AttributeConversionError::BufferViewNotFound(
+                semantic.to_string(),
+            ));
+        }
+    };
+
+    let buffer = match buffer_view.buffer(&context.doc.0) {
+        Some(buffer) => buffer,
+        None => {
+            return Err(AttributeConversionError::BufferNotFound(
+                semantic.to_string(),
+            ));
+        }
+    };
+
+    let iter = accessor.iter(&context.doc.0, &buffer_view, &buffer)?;
+
+    let values = match conversion {
+        ConversionMode::Any => convert_any_values(iter)?,
+        ConversionMode::Rgba => convert_rgba_values(iter)?,
+        ConversionMode::JointIndex => convert_joint_index_values(iter)?,
+        ConversionMode::JointWeight => convert_joint_weight_values(iter)?,
+        ConversionMode::TexCoord => convert_tex_coord_values(iter)?,
+    };
+
+    let format = VertexFormat::from(&values);
+
+    if attribute.format == format {
+        Ok((attribute, values))
+    } else {
+        Err(AttributeConversionError::WrongFormat(
+            semantic.to_string(),
+            format,
+            attribute.name.to_string(),
+            attribute.format,
+        ))
+    }
+}
+
 /// Materializes values for any supported format of vertex attribute
-fn convert_any_values(iter: AccessorIter) -> Result<VertexAttributeValues, ImportPrimitiveError> {
+fn convert_any_values(
+    iter: AccessorIter,
+) -> Result<VertexAttributeValues, AttributeConversionError> {
     match iter {
         AccessorIter::F32(iter) => Ok(VertexAttributeValues::Float32(iter.collect())),
         AccessorIter::U32(iter) => Ok(VertexAttributeValues::Uint32(iter.collect())),
@@ -143,7 +195,7 @@ fn convert_any_values(iter: AccessorIter) -> Result<VertexAttributeValues, Impor
                 Ok(VertexAttributeValues::Uint8x4(iter.collect()))
             }
         }
-        iter => Err(ImportPrimitiveError::UnsupportedAttributeFormat(
+        iter => Err(AttributeConversionError::UnsupportedAttributeFormat(
             iter.component_type(),
             iter.element_type(),
         )),
@@ -151,7 +203,9 @@ fn convert_any_values(iter: AccessorIter) -> Result<VertexAttributeValues, Impor
 }
 
 /// Materializes RGBA values, converting compatible formats to Float32x4
-fn convert_rgba_values(iter: AccessorIter) -> Result<VertexAttributeValues, ImportPrimitiveError> {
+fn convert_rgba_values(
+    iter: AccessorIter,
+) -> Result<VertexAttributeValues, AttributeConversionError> {
     match (iter, iter.normalized()) {
         (AccessorIter::U8x3(iter), true) => Ok(VertexAttributeValues::Float32x4(
             ReadColors::RgbU8(iter).into_rgba_f32().collect(),
@@ -175,7 +229,7 @@ fn convert_rgba_values(iter: AccessorIter) -> Result<VertexAttributeValues, Impo
 /// Materializes joint index values, converting compatible formats to Uint16x4
 fn convert_joint_index_values(
     iter: AccessorIter,
-) -> Result<VertexAttributeValues, ImportPrimitiveError> {
+) -> Result<VertexAttributeValues, AttributeConversionError> {
     match (iter, iter.normalized()) {
         (AccessorIter::U8x4(iter), false) => Ok(VertexAttributeValues::Uint16x4(
             ReadJoints::U8(iter).into_u16().collect(),
@@ -187,7 +241,7 @@ fn convert_joint_index_values(
 /// Materializes joint weight values, converting compatible formats to Float32x4
 fn convert_joint_weight_values(
     iter: AccessorIter,
-) -> Result<VertexAttributeValues, ImportPrimitiveError> {
+) -> Result<VertexAttributeValues, AttributeConversionError> {
     match (iter, iter.normalized()) {
         (AccessorIter::U8x4(iter), true) => Ok(VertexAttributeValues::Float32x4(
             ReadWeights::U8(iter).into_f32().collect(),
@@ -202,7 +256,7 @@ fn convert_joint_weight_values(
 /// Materializes texture coordinate values, converting compatible formats to Float32x2
 fn convert_tex_coord_values(
     iter: AccessorIter,
-) -> Result<VertexAttributeValues, ImportPrimitiveError> {
+) -> Result<VertexAttributeValues, AttributeConversionError> {
     match (iter, iter.normalized()) {
         (AccessorIter::U8x2(iter), true) => Ok(VertexAttributeValues::Float32x2(
             ReadTexCoords::U8(iter).into_f32().collect(),
