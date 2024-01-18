948beda18b05d52b4ebd2c971b678a6ee4ae8089
Kayh <basedlainpilled@gmail.com>
2024-01-18 10:30:27 -0500

    set up omi_physics_shape

--- gltf_kun/src/extensions/omi_physics_shape/mod.rs
+++ gltf_kun/src/extensions/omi_physics_shape/mod.rs
@@ -1,2 +1,26 @@
-#[derive(Debug)]
+use crate::graph::gltf::GltfGraph;
+
+use self::physics_shape::{PhysicsShape, PhysicsShapeWeight};
+
+use super::Extension;
+
+pub mod io;
+pub mod physics_shape;
+
+const EXTENSION_NAME: &str = "OMI_physics_shape";
+
+#[derive(Clone, Debug)]
 pub struct OMIPhysicsShapeExtension;
+
+impl OMIPhysicsShapeExtension {
+    pub fn create_shape(graph: &mut GltfGraph, weight: PhysicsShapeWeight) -> PhysicsShape {
+        let body = PhysicsShape::new(graph, weight);
+        body
+    }
+}
+
+impl Extension<PhysicsShapeWeight> for OMIPhysicsShapeExtension {
+    fn name(&self) -> &'static str {
+        EXTENSION_NAME
+    }
+}
---
+++ gltf_kun/src/extensions/omi_physics_shape/physics_shape.rs
@@ -0,0 +1,63 @@
+use petgraph::stable_graph::NodeIndex;
+use serde::{Deserialize, Serialize};
+
+use crate::{
+    extensions::{Extension, ExtensionProperty},
+    graph::gltf::{GltfGraph, Weight},
+};
+
+use super::OMIPhysicsShapeExtension;
+
+#[derive(Debug, Deserialize, Serialize)]
+pub enum PhysicsShapeWeight {
+    Box(BoxShape),
+    Sphere(SphereShape),
+    Capsule(CapsuleShape),
+    Cylinder(CylinderShape),
+    Convex,
+    Trimesh,
+}
+
+#[derive(Debug, Default, Deserialize, Serialize)]
+pub struct BoxShape {
+    pub size: [f32; 3],
+}
+
+#[derive(Debug, Default, Deserialize, Serialize)]
+pub struct SphereShape {
+    pub radius: f32,
+}
+
+#[derive(Debug, Default, Deserialize, Serialize)]
+pub struct CapsuleShape {
+    pub radius: f32,
+    pub height: f32,
+}
+
+#[derive(Debug, Default, Deserialize, Serialize)]
+pub struct CylinderShape {
+    pub radius: f32,
+    pub height: f32,
+}
+
+#[derive(Copy, Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct PhysicsShape(pub NodeIndex);
+
+impl PhysicsShape {
+    pub fn new(graph: &mut GltfGraph, weight: PhysicsShapeWeight) -> Self {
+        let index = graph.add_node(Weight::Other(
+            OMIPhysicsShapeExtension.encode_property(weight),
+        ));
+        Self(index)
+    }
+}
+
+impl ExtensionProperty<PhysicsShapeWeight> for PhysicsShape {
+    fn index(&self) -> NodeIndex {
+        self.0
+    }
+
+    fn extension(&self) -> &dyn Extension<PhysicsShapeWeight> {
+        &OMIPhysicsShapeExtension
+    }
+}
