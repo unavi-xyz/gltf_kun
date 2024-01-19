8b22a0a46a1b1259ba1e4470b5e2fa46bf56b137
Kayh <basedlainpilled@gmail.com>
2024-01-19 12:59:49 -0500

    add omi_physics_shape io

--- gltf_kun/src/extensions/omi_physics_body/io.rs
+++ gltf_kun/src/extensions/omi_physics_body/io.rs
@@ -1,8 +1,11 @@
 use std::error::Error;
 
-use crate::{graph::gltf::document::GltfDocument, io::format::gltf::GltfFormat};
+use crate::{
+    graph::{gltf::document::GltfDocument, ByteNode, Property},
+    io::format::gltf::GltfFormat,
+};
 
-use super::{ExtensionIO, OMIPhysicsBodyExtension, EXTENSION_NAME};
+use super::{ExtensionIO, OMIPhysicsBodyExtension, PhysicsBodyWeight, EXTENSION_NAME};
 
 impl ExtensionIO<GltfDocument, GltfFormat> for OMIPhysicsBodyExtension {
     fn name(&self) -> &'static str {
@@ -11,38 +14,62 @@ impl ExtensionIO<GltfDocument, GltfFormat> for OMIPhysicsBodyExtension {
 
     fn export(
         &self,
-        _graph: &mut crate::graph::Graph,
-        _doc: &GltfDocument,
-        _format: &mut GltfFormat,
+        graph: &mut crate::graph::Graph,
+        doc: &GltfDocument,
+        format: &mut GltfFormat,
     ) -> Result<(), Box<dyn Error>> {
-        todo!()
+        doc.nodes(graph)
+            .iter()
+            .enumerate()
+            .filter_map(|(i, n)| {
+                n.get_extension::<OMIPhysicsBodyExtension>(graph)
+                    .map(|ext| (i, ext.read(graph)))
+            })
+            .for_each(|(i, weight)| {
+                let node = format
+                    .json
+                    .nodes
+                    .get_mut(i)
+                    .expect("Node index out of bounds");
+
+                let extensions = node
+                    .extensions
+                    .get_or_insert(gltf::json::extensions::scene::Node::default());
+
+                extensions.others.insert(
+                    EXTENSION_NAME.to_string(),
+                    serde_json::to_value(weight).expect("Failed to serialize extension"),
+                );
+            });
+
+        Ok(())
     }
 
     fn import(
         &self,
-        _graph: &mut crate::graph::Graph,
-        _format: &mut GltfFormat,
-        _doc: &GltfDocument,
+        graph: &mut crate::graph::Graph,
+        format: &mut GltfFormat,
+        doc: &GltfDocument,
     ) -> Result<(), Box<dyn Error>> {
-        todo!()
-    }
-}
+        format
+            .json
+            .nodes
+            .iter()
+            .enumerate()
+            .filter_map(|(i, n)| n.extensions.as_ref().map(|e| (i, e)))
+            .filter_map(|(i, e)| e.others.get(EXTENSION_NAME).map(|v| (i, v)))
+            .filter_map(|(i, v)| {
+                serde_json::from_value::<PhysicsBodyWeight>(v.clone())
+                    .ok()
+                    .map(|w| (i, w))
+            })
+            .for_each(|(i, weight)| {
+                let nodes = doc.nodes(graph);
+                let node = nodes.get(i).expect("Node index out of bounds");
+                let mut ext = node.create_extension::<OMIPhysicsBodyExtension>(graph);
+                ext.write(graph, &weight);
+            });
 
-#[cfg(test)]
-mod tests {
-    use crate::{
-        extensions::omi_physics_body::OMIPhysicsBodyExtension,
-        graph::{Graph, Property},
-    };
-
-    use super::*;
-
-    #[test]
-    fn test_io() {
-        let mut graph = Graph::default();
-        let doc = GltfDocument::new(&mut graph);
-        let node = doc.create_node(&mut graph);
-
-        node.create_extension::<OMIPhysicsBodyExtension>(&mut graph);
+        Ok(())
     }
 }
