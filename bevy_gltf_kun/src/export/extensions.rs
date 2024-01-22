d6d5ec479178a7b62c6cabe241e3d0ef34d68e4b
Kayh <basedlainpilled@gmail.com>
2024-01-22 14:03:00 -0500

    bevy extension export system piping

--- bevy_gltf_kun/src/export/extensions.rs
+++ bevy_gltf_kun/src/export/extensions.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{ecs::system::RunSystemOnce, prelude::*};
 use gltf_kun::{
     extensions::{omi_physics_body::OMIPhysicsBody, DefaultExtensions},
     graph::gltf::document::GltfDocument,
@@ -7,17 +7,15 @@ use gltf_kun::{
 use super::gltf::ExportContext;
 
 pub trait BevyExtensionExport {
-    fn bevy_export(context: &mut ExportContext);
-}
-
-pub trait BevyExportExtensions<D>: Send + Sync + 'static {
     fn bevy_export(context: In<ExportContext>) -> ExportContext;
 }
 
-impl BevyExportExtensions<GltfDocument> for DefaultExtensions {
-    fn bevy_export(In(mut context): In<ExportContext>) -> ExportContext {
-        OMIPhysicsBody::bevy_export(&mut context);
+pub trait BevyExportExtensions<D>: Send + Sync + 'static {
+    fn bevy_export(context: In<ExportContext>, world: &mut World) -> ExportContext;
+}
 
-        context
+impl BevyExportExtensions<GltfDocument> for DefaultExtensions {
+    fn bevy_export(In(context): In<ExportContext>, world: &mut World) -> ExportContext {
+        world.run_system_once_with(context, OMIPhysicsBody::bevy_export)
     }
 }
--- bevy_gltf_kun/src/extensions/omi_physics/export.rs
+++ bevy_gltf_kun/src/extensions/omi_physics/export.rs
@@ -4,8 +4,9 @@ use gltf_kun::extensions::omi_physics_body::OMIPhysicsBody;
 use crate::export::{extensions::BevyExtensionExport, gltf::ExportContext};
 
 impl BevyExtensionExport for OMIPhysicsBody {
-    fn bevy_export(context: &mut ExportContext) {
+    fn bevy_export(In(context): In<ExportContext>) -> ExportContext {
         info!("Exporting OMI Physics Body");
+
+        context
     }
 }
-
