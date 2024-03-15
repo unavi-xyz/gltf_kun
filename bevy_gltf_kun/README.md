f5ea42b33c1f47fd43859e8335f3ae0724928e9f
Kayh <basedlainpilled@gmail.com>
2024-03-15 13:33:38 -0400

    update bevy_gltf_kun docs

--- bevy_gltf_kun/README.md
+++ bevy_gltf_kun/README.md
@@ -1,11 +1,3 @@
 # bevy_gltf_kun
 
-Bevy glTF plugin using [gltf_kun](https://crates.io/crates/gltf_kun).
-
-## Comparison
-
-Compared to `bevy_gltf`, this plugin adds support for:
-
-- Arbitrary glTF extensions
-- glTF exporting
-- [glXF](https://github.com/KhronosGroup/glXF) import and export
+<!-- cargo-rdme -->
--- bevy_gltf_kun/src/lib.rs
+++ bevy_gltf_kun/src/lib.rs
@@ -1,3 +1,13 @@
+//! Bevy glTF plugin using [gltf_kun](https://crates.io/crates/gltf_kun).
+//!
+//! ## Comparison
+//!
+//! Compared to `bevy_gltf`, this plugin adds support for:
+//!
+//! - Arbitrary glTF extensions
+//! - glTF exporting
+//! - [glXF](https://github.com/KhronosGroup/glXF) import and export
+
 #[cfg(feature = "export")]
 pub mod export;
 pub mod extensions;
