8ad6029baeda6f307da684b6c7218a3057698990
Kayh <basedlainpilled@gmail.com>
2024-04-01 11:50:47 -0400

    fix wasm build

--- crates/bevy_gltf_kun/src/export/extensions.rs
+++ crates/bevy_gltf_kun/src/export/extensions.rs
@@ -1,4 +1,4 @@
-use bevy::{ecs::system::RunSystemOnce, prelude::*};
+use bevy::prelude::*;
 use gltf_kun::{extensions::DefaultExtensions, graph::gltf::GltfDocument};
 
 use super::gltf::ExportContext;
@@ -12,13 +12,17 @@ pub trait BevyExportExtensions<D>: Send + Sync + 'static {
 }
 
 impl BevyExportExtensions<GltfDocument> for DefaultExtensions {
-    fn bevy_export(In(context): In<ExportContext>, world: &mut World) -> ExportContext {
+    fn bevy_export(In(mut context): In<ExportContext>, world: &mut World) -> ExportContext {
         #[cfg(feature = "omi_physics")]
         {
-            world.run_system_once_with(
+            use bevy::ecs::system::RunSystemOnce;
+
+            context = world.run_system_once_with(
                 context,
                 gltf_kun::extensions::omi_physics_body::OmiPhysicsBody::bevy_export,
-            )
+            );
         }
+
+        context
     }
 }
