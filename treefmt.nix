653bde1dc9d262c6d4894b1c972abd7e6d65bfb6
Kayh <basedlainpilled@gmail.com>
2025-07-04 05:24:35 -0400

    strict nixfmt

--- treefmt.nix
+++ treefmt.nix
@@ -1,15 +1,14 @@
-_:
-{
+_: {
   projectRootFile = "flake.nix";
   programs = {
     actionlint.enable = true;
     deadnix.enable = true;
     mdformat.enable = true;
-    nixfmt.enable = true;
-    rustfmt = {
+    nixfmt = {
       enable = true;
-      edition = "2024";
+      strict = true;
     };
+    rustfmt.enable = true;
     statix.enable = true;
     taplo.enable = true;
     yamlfmt.enable = true;
